e200d620c768cb367783556e5f437813335cd7b3
Vijaykumar Pandurang Mangore <138985391+vijaymangore@users.noreply.github.com>
2024-11-21 15:56:29 +0530

    Add files via upload

--- ai_review.py
+++ ai_review.py
@@ -1,30 +1,24 @@
 import streamlit as st
 import google.generativeai as ai
 import os
-f=open(r"C:/Users/ADMIN/Innomatics/genai/keys/Generative AI.txt")
+f=open(r"C:/Users/ADMIN/Innomatics/genai/keys/gkey.txt")
 key=f.read()
 print(key)
 ai.configure(api_key= key)
-
-
 # System prompt for the AI Code Reviewer
 sys_prompt = """You are an AI Code Reviewer. 
 When given a piece of Python code, analyze it carefully to identify any bugs or errors.
 Provide suggestions to fix them. Your response must follow this structured format:
 1. provide a sub-heading "Bug Report" (bold). 2. In the "Bug Report" section, list all the identified bugs or errors in a clear, numbered format (e.g., 1, 2, 3).
 3. Next, provide a sub-heading "Fixed Code" (bold). 4. Under "Fixed Code," rewrite the original code with all the identified issues fixed.
-
 Always maintain proper formatting and clarity in your response and to the point only.Do explain 'how' and 'why'. Use this format only.
 """
-
 # List available models (optional, just for debugging purposes)
 available_models = ai.list_models()
-for model in available_models:
-    print(model.name)
-
+# for model in available_models:
+#     print(model.name)
 # Define the Generative AI model
-
-    model = ai.GenerativeModel(
+model = ai.GenerativeModel(
         model_name="models/gemini-1.5-flash",  
         system_instruction=sys_prompt,
     )
